fb07dde389d9df41e2d29e944cd82c8193c3550d
Padjshah <104107048+Padjshah@users.noreply.github.com>
2022-04-24 18:28:39 +0300

    Create caesar.c

---
+++ Week2/caesar.c
@@ -0,0 +1,64 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h> //strlen
+#include <ctype.h>
+#include <stdlib.h> //atoi
+
+int main(int argc, string argv[])
+{
+    
+    if (argc != 2 || argv[1][0] == '-') //just needs one key
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    else
+    {
+        for (int i = 0, lengthOfKey = strlen(argv[1]) ; i < lengthOfKey ; i++) //control if there is alphabetic %c in key
+        {
+            if ( argv[1][i] < '0' || argv[1][i] > '9' )
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+        }
+
+    }
+    int key = atoi(argv[1]); //atoi func makes string to be int
+    key %= 26; //
+
+    string plainText = get_string("Plain text: ");
+    printf("Cypher text: ");
+
+    for (int i = 0, lengthOfText = strlen(plainText) ; i < lengthOfText ; i++)
+    {
+        if ( isalpha(plainText[i]) )
+        {
+            while (plainText[i] >= 'A' && plainText[i] <= 'Z')
+            {
+                if (plainText[i] + key > 90)
+                    printf("%c", plainText[i] + key - 26);
+                else
+                    printf("%c", plainText[i] + key);
+                
+                break;
+            }
+
+            while (plainText[i] >= 'a' && plainText[i] <= 'z')
+            {
+                if (plainText[i] + key > 122)
+                    printf("%c", plainText[i] + key - 26);
+                else
+                    printf("%c", plainText[i] + key);
+                
+                break;
+            }
+        }
+      
+        else
+            printf("%c", plainText[i]);
+      
+    }
+    printf("\n");
+}
